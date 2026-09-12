b61d4470e26be103a8e98ff5795c7cc7dccabc4a
Metin Taha Yılmaz <172074607+metintahayilmaz@users.noreply.github.com>
2026-09-13 00:32:57 +0300

    Initial Niodome site

--- hooks/useMxdMenuGsap.ts
+++ hooks/useMxdMenuGsap.ts
@@ -13,8 +13,8 @@ import {
 
 const N_HEADER = 3;
 const N_MAIN_SPANS = 10;
-const N_CONTACT = 8;
-const N_FOOTER = 4;
+const N_CONTACT = 6;
+const N_FOOTER = 3;
 const N_DIVIDERS = 6;
 const N_ARROWS = 4;
 const N_ROWS = 5;
